a4df7ca3d230cff7db54997bb066c6f8ff9f3722
Champ <chinnawat.ongart@gmail.com>
2017-05-07 19:18:46 +0700

    add test for CreateLine class

---
+++ test/testCreateLine.js
@@ -0,0 +1,9 @@
+let should = require('should')
+const CreateLine = require('../src/CreateLine')
+
+describe('Test CreateLine class', function() {
+  it('can createLineList', function() {
+    let createLine = new CreateLine({NUMBER_OF_LINE:3})
+    createLine.numLineList.should.eql([1,2,3])
+  })
+})
