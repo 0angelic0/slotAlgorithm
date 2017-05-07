aaec8a2a39a04ffeb4eb69e288292fd3bb876303
Champ <chinnawat.ongart@gmail.com>
2017-05-07 19:37:35 +0700

    module exports CreateFace

---
+++ src/CreateFace.js
@@ -0,0 +1,21 @@
+class CreateFace{
+    constructor(props){
+        if(props){
+            this.NUMBER_OF_COLUMNS = props.NUMBER_OF_COLUMNS
+            this.NUMBER_OF_FACE = props.NUMBER_OF_FACE
+            this.faceList = this.createFaceAndNumFace(this.NUMBER_OF_FACE,this.NUMBER_OF_COLUMNS)
+        }
+    }
+    createFaceAndNumFace(numFaces,numCol){
+        let temp = []
+        for(let i = 3; i<=numCol; i++){
+            for(let j=1;j<=numFaces;j++){
+                temp.push({FACE:j,NUM_OF_FACE:i})
+            }
+        }
+        return temp
+    }
+
+}
+
+module.exports = CreateFace
