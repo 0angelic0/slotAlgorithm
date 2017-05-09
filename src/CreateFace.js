34ce69824ec8274dfb4cc158614bd30a8f6693b1
Champ <chinnawat.ongart@gmail.com>
2017-05-09 12:15:30 +0700

    modifed createFaceAndNumFaces object and tested

--- src/CreateFace.js
+++ src/CreateFace.js
@@ -2,15 +2,16 @@ const Slot = require('./Slot')
 class CreateFace {
     constructor(slot){
         this.slot = slot
-        this.faceList = this.createFaceAndNumFace(this.slot.NUMBER_OF_FACE,this.slot.NUMBER_OF_COLUMNS)
+        this.multiplierTable = slot.multiplierTable //[0,5,10,20,50,100,200,500,1000]
+        this.faceList = this.createFaceAndNumFace(this.slot.NUMBER_OF_FACE,this.slot.NUMBER_OF_COLUMNS,this.multiplierTable)
     }
-    createFaceAndNumFace(numFaces,numCol){
+    createFaceAndNumFace(numFaces,numCol,multiplierTable){
         let temp = []
         for(let i = 3; i<=numCol; i++){
             for(let j=1;j<=numFaces;j++){
                 let face = j
                 let numOfFace = i
-                temp.push({FACE:face,NUM_OF_FACE:numOfFace})
+                temp.push({FACE:face,NUM_OF_FACE:numOfFace,MULTIPLIER:multiplierTable[j]})
             }
         }
         return temp
--- test/testCreateFace.js
+++ test/testCreateFace.js
@@ -3,20 +3,20 @@ const CreateFace = require('../src/CreateFace')
 const Slot = require('../src/Slot')
 describe('Test CreateFace class',function(){
     it('can createFaceAndNumFace',function(){
-        let slot = new Slot({NUMBER_OF_COLUMNS:5,NUMBER_OF_FACE:3})
+        let slot = new Slot({NUMBER_OF_COLUMNS:5,NUMBER_OF_FACE:3,MULTIPLIER_TABLE:[0,5,10,20,50,100,200,500,1000]})
         let createFace = new CreateFace(slot)
         let obj = [
-            {FACE:1,NUM_OF_FACE:3},
-            {FACE:2,NUM_OF_FACE:3},
-            {FACE:3,NUM_OF_FACE:3},
+            {FACE:1,NUM_OF_FACE:3,MULTIPLIER:5},
+            {FACE:2,NUM_OF_FACE:3,MULTIPLIER:10},
+            {FACE:3,NUM_OF_FACE:3,MULTIPLIER:20},
             
-            {FACE:1,NUM_OF_FACE:4},
-            {FACE:2,NUM_OF_FACE:4},
-            {FACE:3,NUM_OF_FACE:4},
+            {FACE:1,NUM_OF_FACE:4,MULTIPLIER:5},
+            {FACE:2,NUM_OF_FACE:4,MULTIPLIER:10},
+            {FACE:3,NUM_OF_FACE:4,MULTIPLIER:20},
             
-            {FACE:1,NUM_OF_FACE:5},
-            {FACE:2,NUM_OF_FACE:5},
-            {FACE:3,NUM_OF_FACE:5},
+            {FACE:1,NUM_OF_FACE:5,MULTIPLIER:5},
+            {FACE:2,NUM_OF_FACE:5,MULTIPLIER:10},
+            {FACE:3,NUM_OF_FACE:5,MULTIPLIER:20},
             
         ]
         createFace.faceList.should.eql(obj)
